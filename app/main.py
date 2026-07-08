54f69979981aa0df6a65fd80eb2aa9fdd9d3c260
thomastegean-hash <thomas.tegean@gmail.com>
2026-07-08 21:30:56 +0000

    modified api to serve static files

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.routes import router
 
 app = FastAPI()
 
 app.include_router(router)
+
+app.include_router(router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+async def home():
+    return FileResponse("static/index.html")
